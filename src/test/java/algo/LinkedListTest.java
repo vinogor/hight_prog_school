6eaad0325a35402b80f31b733181f561ad39aebc
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2020-02-12 09:09:30 +0300

    commit 1

---
+++ src/test/java/algo/LinkedListTest.java
@@ -0,0 +1,22 @@
+package algo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+class LinkedListTest {
+
+    @Before
+    void setUp() {
+    }
+
+    @After
+    void tearDown() {
+    }
+
+    @Test
+    void addInTail() {
+
+    }
+}
