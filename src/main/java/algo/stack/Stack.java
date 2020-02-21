3459bf54efaeec7aecf49010000bacc97d6c7346
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2020-02-21 09:04:06 +0300

    Stack part 1

--- src/main/java/algo/stack/Stack.java
+++ src/main/java/algo/stack/Stack.java
@@ -2,27 +2,48 @@ package algo.stack;
 
 //
 
+import algo.linkedList.LinkedList;
+import algo.linkedList.Node;
+
 public class Stack<T> {
+
+    public LinkedList<T> ll;
+    public T lastPushedValue;
+
     public Stack() {
         // инициализация внутреннего хранилища стека
+        ll = new LinkedList<>();
     }
 
-    public int size() {
-        // размер текущего стека
-        return 0;
-    }
-
-    public T pop() {
-        // ваш код
-        return null;  // если стек пустой
-    }
-
+    // втолкнуть на вершину
     public void push(T val) {
-        // ваш код
+        ll.addInTail(new Node<>(val));
     }
 
+    // вытолкнуть с вершины (с удалением)
+    public T pop() {
+
+        // если стек пустой то выталкивать нечего
+        if (ll.countPro() == 0) {
+            return null;
+        }
+
+        lastPushedValue = ll.head.value;
+        ll.remove(lastPushedValue);
+
+        return lastPushedValue;
+    }
+
+    // получить с вершины (без удаления)
     public T peek() {
-        // ваш код
+
         return null; // если стек пустой
     }
+
+    // размер текущего стека
+    public int size() {
+        // счётчик возьмём из линкед листа, он уже реализован
+        return ll.countPro();
+    }
+
 }
