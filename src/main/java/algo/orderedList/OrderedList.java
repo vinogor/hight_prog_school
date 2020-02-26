b2be4c3db442867e511305c05eb98f536f27b810
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-02-26 14:53:34 +0300

    method add + tests

--- src/main/java/algo/orderedList/OrderedList.java
+++ src/main/java/algo/orderedList/OrderedList.java
@@ -14,9 +14,11 @@ class Node<T> {
     }
 }
 
+// на основе двусвязного списка
 public class OrderedList<T> {
+
     public Node<T> head, tail;
-    private boolean _ascending;
+    private boolean _ascending; // по возрастанию
 
     public OrderedList(boolean asc) {
         head = null;
@@ -27,21 +29,75 @@ public class OrderedList<T> {
     // -1 если v1 < v2
     // 0 если v1 == v2
     // +1 если v1 > v2
-    public int compare(T v1, T v2) {
 
+    //  Если это числа, их можно сравнить арифметически,
+    //  если это строки, то их надо перед стандартным в любом языке
+    //  лексикографическом сравнением очистить от начальных и конечных пробелов.
+
+    // ??? Пока сделайте базовый вариант этого метода, который сравнивает числовые значения.
+
+    public int compare(T v1, T v2) {
+        if (v1 instanceof Integer && v2 instanceof Integer) {
+            if (v1.equals(v2)) { // так как пул
+                return 0;
+            } else {
+                return (Integer) v1 > (Integer) v2 ? 1 : -1;
+            }
+        }
         return 0;
     }
 
     // автоматическая вставка value
     // в нужную позицию
     public void add(T value) {
+        Node<T> newNode = new Node<>(value);
 
+        // если список пуст то просто вставляем
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else
+            // если вставка в самое начало
+            if ((compare(head.value, value) >= 0 && _ascending)
+                    || (compare(head.value, value) <= 0 && !_ascending)) {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            } else
+                // если вставка в конец
+                if ((compare(tail.value, value) <= 0 && _ascending)
+                        || (compare(tail.value, value) >= 0 && !_ascending)) {
+                    newNode.prev = tail;
+                    tail.next = newNode;
+                    tail = newNode;
+                }
+                // в остальных случаях проходимся в поисках места
+                // и вставляем между узлами
+                else {
+                    Node<T> node = this.head;
+                    while (true) { // так как хвост мы уже сравнили
+                        if ((_ascending && (compare(node.value, value) <= 0) && (compare(value, node.next.value) <= 0))
+                                || (!_ascending && (compare(node.value, value) >= 0) && (compare(value, node.next.value) >= 0))) {
+
+                            // вставляем ПОСЛЕ текущей
+                            newNode.prev = node;
+                            newNode.next = node.next;
+                            node.next.prev = newNode;
+                            node.next = newNode;
+                            break;
+                        }
+                        node = node.next;
+                    }
+                }
     }
 
+    // Переделайте функцию поиска с учётом признака упорядоченности и возможности раннего прерывания поиска,
+    // если найден заведомо больший или меньший элемент, нежели искомый (то есть значение по сути не найдено).
     public Node<T> find(T val) {
-        return null; // здесь будет ваш код
+        return null;
     }
 
+    //
     public void delete(T val) {
         // здесь будет ваш код
     }
@@ -55,9 +111,9 @@ public class OrderedList<T> {
         return 0; // здесь будет ваш код подсчёта количества элементов в списке
     }
 
-    ArrayList<Node<T>> getAll() // выдать все элементы упорядоченного
-    // списка в виде стандартного списка
-    {
+    ArrayList<Node<T>> getAll() {
+        // выдать все элементы упорядоченного
+        // списка в виде стандартного списка
         ArrayList<Node<T>> r = new ArrayList<Node<T>>();
         Node<T> node = head;
         while (node != null) {
---
+++ src/test/java/algo/orderedList/OrderedListTest.java
@@ -0,0 +1,291 @@
+package algo.orderedList;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class OrderedListTest {
+
+    private static OrderedList<Integer> ol;
+
+    @Test
+    public void add_asc_01() {
+        ol = new OrderedList<>(true);
+        assertThat(ol.getAll().size(), is(0));
+    }
+
+    @Test
+    public void add_asc_02() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+
+        assertThat(ol.getAll().size(), is(1));
+        assertThat(ol.getAll().get(0).value, is(0));
+    }
+
+    @Test
+    public void add_asc_03() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+        ol.add(1);
+
+        assertThat(ol.getAll().size(), is(2));
+        assertThat(ol.getAll().get(0).value, is(0));
+        assertThat(ol.getAll().get(1).value, is(1));
+    }
+
+    @Test
+    public void add_asc_04() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+        ol.add(1);
+        ol.add(2);
+
+        assertThat(ol.getAll().size(), is(3));
+        assertThat(ol.getAll().get(0).value, is(0));
+        assertThat(ol.getAll().get(1).value, is(1));
+        assertThat(ol.getAll().get(2).value, is(2));
+    }
+
+    @Test
+    public void add_asc_05() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+        ol.add(-1);
+
+        assertThat(ol.getAll().size(), is(2));
+        assertThat(ol.getAll().get(0).value, is(-1));
+        assertThat(ol.getAll().get(1).value, is(0));
+    }
+
+    @Test
+    public void add_asc_06() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+        ol.add(-1);
+        ol.add(-2);
+
+        assertThat(ol.getAll().size(), is(3));
+        assertThat(ol.getAll().get(0).value, is(-2));
+        assertThat(ol.getAll().get(1).value, is(-1));
+        assertThat(ol.getAll().get(2).value, is(0));
+    }
+
+    @Test
+    public void add_asc_07() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+        ol.add(1);
+        ol.add(2);
+        ol.add(0);
+
+        assertThat(ol.getAll().size(), is(4));
+        assertThat(ol.getAll().get(0).value, is(0));
+        assertThat(ol.getAll().get(1).value, is(0));
+        assertThat(ol.getAll().get(2).value, is(1));
+        assertThat(ol.getAll().get(3).value, is(2));
+    }
+
+    @Test
+    public void add_asc_08() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+        ol.add(1);
+        ol.add(2);
+        ol.add(1);
+
+        assertThat(ol.getAll().size(), is(4));
+        assertThat(ol.getAll().get(0).value, is(0));
+        assertThat(ol.getAll().get(1).value, is(1));
+        assertThat(ol.getAll().get(2).value, is(1));
+        assertThat(ol.getAll().get(3).value, is(2));
+    }
+
+    @Test
+    public void add_asc_09() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+        ol.add(1);
+        ol.add(2);
+        ol.add(2);
+
+        assertThat(ol.getAll().size(), is(4));
+        assertThat(ol.getAll().get(0).value, is(0));
+        assertThat(ol.getAll().get(1).value, is(1));
+        assertThat(ol.getAll().get(2).value, is(2));
+        assertThat(ol.getAll().get(3).value, is(2));
+    }
+
+    @Test
+    public void add_asc_10() {
+        ol = new OrderedList<>(true);
+        ol.add(0);
+        ol.add(7);
+        ol.add(6);
+        ol.add(3);
+        ol.add(1);
+        ol.add(100);
+        ol.add(-100);
+
+        assertThat(ol.getAll().size(), is(7));
+        assertThat(ol.getAll().get(0).value, is(-100));
+        assertThat(ol.getAll().get(1).value, is(0));
+        assertThat(ol.getAll().get(2).value, is(1));
+        assertThat(ol.getAll().get(3).value, is(3));
+        assertThat(ol.getAll().get(4).value, is(6));
+        assertThat(ol.getAll().get(5).value, is(7));
+        assertThat(ol.getAll().get(6).value, is(100));
+    }
+
+
+    @Test
+    public void add_dec_01() {
+        ol = new OrderedList<>(false);
+        assertThat(ol.getAll().size(), is(0));
+    }
+
+    @Test
+    public void add_dec_02() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+
+        assertThat(ol.getAll().size(), is(1));
+        assertThat(ol.getAll().get(0).value, is(0));
+    }
+
+    @Test
+    public void add_dec_03() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+        ol.add(1);
+
+        assertThat(ol.getAll().size(), is(2));
+        assertThat(ol.getAll().get(0).value, is(1));
+        assertThat(ol.getAll().get(1).value, is(0));
+    }
+
+    @Test
+    public void add_dec_04() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+        ol.add(1);
+        ol.add(2);
+
+        assertThat(ol.getAll().size(), is(3));
+        assertThat(ol.getAll().get(0).value, is(2));
+        assertThat(ol.getAll().get(1).value, is(1));
+        assertThat(ol.getAll().get(2).value, is(0));
+    }
+
+    @Test
+    public void add_dec_05() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+        ol.add(-1);
+
+        assertThat(ol.getAll().size(), is(2));
+        assertThat(ol.getAll().get(0).value, is(0));
+        assertThat(ol.getAll().get(1).value, is(-1));
+    }
+
+    @Test
+    public void add_dec_06() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+        ol.add(-1);
+        ol.add(-2);
+
+        assertThat(ol.getAll().size(), is(3));
+        assertThat(ol.getAll().get(0).value, is(0));
+        assertThat(ol.getAll().get(1).value, is(-1));
+        assertThat(ol.getAll().get(2).value, is(-2));
+    }
+
+    @Test
+    public void add_dec_07() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+        ol.add(1);
+        ol.add(2);
+        ol.add(0);
+
+        assertThat(ol.getAll().size(), is(4));
+        assertThat(ol.getAll().get(0).value, is(2));
+        assertThat(ol.getAll().get(1).value, is(1));
+        assertThat(ol.getAll().get(2).value, is(0));
+        assertThat(ol.getAll().get(3).value, is(0));
+    }
+
+    @Test
+    public void add_dec_08() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+        ol.add(1);
+        ol.add(2);
+        ol.add(1);
+
+        assertThat(ol.getAll().size(), is(4));
+        assertThat(ol.getAll().get(0).value, is(2));
+        assertThat(ol.getAll().get(1).value, is(1));
+        assertThat(ol.getAll().get(2).value, is(1));
+        assertThat(ol.getAll().get(3).value, is(0));
+    }
+
+    @Test
+    public void add_dec_09() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+        ol.add(1);
+        ol.add(2);
+        ol.add(2);
+
+        assertThat(ol.getAll().size(), is(4));
+        assertThat(ol.getAll().get(0).value, is(2));
+        assertThat(ol.getAll().get(1).value, is(2));
+        assertThat(ol.getAll().get(2).value, is(1));
+        assertThat(ol.getAll().get(3).value, is(0));
+    }
+
+    @Test
+    public void add_dec_10() {
+        ol = new OrderedList<>(false);
+        ol.add(0);
+        ol.add(7);
+        ol.add(6);
+        ol.add(3);
+        ol.add(1);
+        ol.add(100);
+        ol.add(-100);
+
+        assertThat(ol.getAll().size(), is(7));
+        assertThat(ol.getAll().get(0).value, is(100));
+        assertThat(ol.getAll().get(1).value, is(7));
+        assertThat(ol.getAll().get(2).value, is(6));
+        assertThat(ol.getAll().get(3).value, is(3));
+        assertThat(ol.getAll().get(4).value, is(1));
+        assertThat(ol.getAll().get(5).value, is(0));
+        assertThat(ol.getAll().get(6).value, is(-100));
+    }
+
+
+    @Test
+    public void find() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void count() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
