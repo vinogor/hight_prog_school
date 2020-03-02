9de1c2af9981c64b5097d9d4bc2329e541a559b8
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-03-02 11:38:21 +0300

    NativeDictionary init2

--- src/main/java/algo/nativeDictionary/NativeDictionary.java
+++ src/main/java/algo/nativeDictionary/NativeDictionary.java
@@ -1,37 +1,53 @@
 package algo.nativeDictionary;
 
+// 1. Выясните, как в языке программирования, которым вы пользуетесь, реализован тип данных Словарь.
+// 2. Реализуйте класс NativeDictionary, как описано выше.
+// 3. Добавьте в этот класс три метода:
+//    - put(key, value) - сохранение внутри класса ассоциативного массива пары ключ-значение по описанной выше схеме;
+//    - is_key(key) - проверка, имеется ли в слотах такой ключ;
+//    - get(key) - поиск и извлечение значения по ключу, или отсутствие значения, если ключ не найден.
+// 4. Сделайте тесты, проверяющие, как работают put(), is_key() и get():
+//    - добавление значения по новому ключу и добавление значения по уже существующему ключу с проверками что записалось,
+//    - проверка присутствующего и отсутствующего ключей,
+//    - извлечение значения по существующему и отсутствующему ключу.
+
 import java.lang.reflect.Array;
 
 class NativeDictionary<T> {
     public int size;
     public String[] slots;
     public T[] values;
+    public int counter;
 
     public NativeDictionary(int sz, Class clazz) {
         size = sz;
         slots = new String[size];
         values = (T[]) Array.newInstance(clazz, this.size);
+        counter = 0;
     }
 
+    // всегда возвращает корректный индекс слота
     public int hashFun(String key) {
-        // всегда возвращает корректный индекс слота
-        return 0;
+        return key.hashCode() % size;
     }
 
+    // возвращает true если ключ имеется,
+    // иначе false
     public boolean isKey(String key) {
-        // возвращает true если ключ имеется,
-        // иначе false
+
         return false;
     }
 
+    // гарантированно записываем
+    // значение value по ключу key
     public void put(String key, T value) {
-        // гарантированно записываем
-        // значение value по ключу key
+
     }
 
+    // возвращает value для key,
+    // или null если ключ не найден
     public T get(String key) {
-        // возвращает value для key,
-        // или null если ключ не найден
+
         return null;
     }
 }
