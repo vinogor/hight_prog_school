50b8f65962d5859f31a9f80ba7a943c5a17bdb07
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-03-05 10:22:51 +0300

    PowerSet init

---
+++ src/main/java/algo/powerSet/PowerSet.java
@@ -0,0 +1,54 @@
+package algo.powerSet;
+
+
+public class PowerSet {
+
+    public PowerSet() {
+        // ваша реализация хранилища
+        // В реализации используйте вариант с фиксированным размером хэш-таблицы на 20,000 значений строкового типа.
+    }
+
+    public int size() {
+        // количество элементов в множестве
+        return 0;
+    }
+
+
+    public void put(String value) {
+        // всегда срабатывает
+    }
+
+    public boolean get(String value) {
+        // возвращает true если value имеется в множестве,
+        // иначе false
+        return false;
+    }
+
+    public boolean remove(String value) {
+        // возвращает true если value удалено
+        // иначе false
+        return false;
+    }
+
+    public PowerSet intersection(PowerSet set2) {
+        // пересечение текущего множества и set2
+        return null;
+    }
+
+    public PowerSet union(PowerSet set2) {
+        // объединение текущего множества и set2
+        return null;
+    }
+
+    public PowerSet difference(PowerSet set2) {
+        // разница текущего множества и set2
+        return null;
+    }
+
+    public boolean isSubset(PowerSet set2) {
+        // возвращает true, если set2 есть
+        // подмножество текущего множества,
+        // иначе false
+        return false;
+    }
+}
