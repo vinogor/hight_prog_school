290e49e38df9b0834cbb3fbf2ec24addc15ed72d
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-03-10 11:56:13 +0300

    тесты к put()

--- src/test/java/algo/powerSet/PowerSetTest.java
+++ src/test/java/algo/powerSet/PowerSetTest.java
@@ -17,15 +17,60 @@ public class PowerSetTest {
     }
 
     @Test
-    public void size() {
-
-//        assertThat(, is());
+    public void put_00() {
+        ps.put(null);
+        assertThat(ps.size(), is(0));
     }
 
     @Test
     public void put_01() {
         assertThat(ps.size(), is(0));
     }
+
+    @Test
+    public void put_02() {
+        ps.put("1");
+
+        assertThat(ps.size(), is(1));
+    }
+
+    @Test
+    public void put_03() {
+        ps.put("1");
+        ps.put("2");
+
+        assertThat(ps.size(), is(2));
+    }
+
+    @Test
+    public void put_04() {
+        ps.put("1");
+        ps.put("1");
+
+        assertThat(ps.size(), is(1));
+    }
+
+    @Test
+    public void put_05() {
+        ps.put("1");
+        ps.put("1");
+        ps.put("1");
+
+        assertThat(ps.size(), is(1));
+    }
+
+    @Test // положить второе уникальное значение с одинаковым хэш
+    public void put_06() {
+        ps.put("1");
+        char ch = "1".toCharArray()[0];
+        ch = (char) (ch + ps.size);
+        ps.put(String.valueOf(ch));
+
+        assertThat(ps.size(), is(2));
+    }
+
+    // а если заполнить полностью?
+
 
     @Test
     public void get() {
