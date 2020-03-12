2c6017f5b6bb11f48dce1453cc7494ea5feee597
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-03-12 16:04:21 +0300

    BloomFilter init

---
+++ src/test/java/algo/bloomFilter/BloomFilterTest.java
@@ -0,0 +1,29 @@
+package algo.bloomFilter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BloomFilterTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void hash1() {
+    }
+
+    @Test
+    public void hash2() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void isValue() {
+    }
+}
