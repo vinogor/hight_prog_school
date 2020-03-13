225c381df010a010caf21df5c141aee01feb27c6
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-03-13 16:20:13 +0300

    BloomFilter + tests

--- src/main/java/algo/bloomFilter/BloomFilter.java
+++ src/main/java/algo/bloomFilter/BloomFilter.java
@@ -1,17 +1,13 @@
 package algo.bloomFilter;
 
-
 public class BloomFilter {
 
     public int filter_len;       // размер в битах
     public int bitArray;         // типо битовый массив для фильтра
 
     public BloomFilter(int f_len) {
-        filter_len = f_len; // будет всё норм работать для максимум = 32 т.к. bitArray - int
+        filter_len = f_len;      // будет всё норм работать для максимум = 32 т.к. bitArray - int
         bitArray = 0b0000_0000_0000_0000_0000_0000_0000_0000;
-
-        System.out.println(getBinaryStringFromInt(bitArray) + " - исх состояние бит массива ");
-        System.out.println();
     }
 
     public int hash1(String str1) {
@@ -36,53 +32,38 @@ public class BloomFilter {
 
     public void add(String str1) {
 
-        System.out.println("    добавляем: " + str1);
-
+//        System.out.println("    добавляем: " + str1);
         int hash1 = hash1(str1);
         bitArray = bitArray | 1 << (hash1);
-        System.out.println(getBinaryStringFromInt(bitArray) + " - бит массив после добавление хэш1 = " + hash1);
+//        System.out.println(getBinaryStringWithShiftFromInt(bitArray) + " - бит массив после добавление хэш1 = " + hash1);
 
         int hash2 = hash2(str1);
         bitArray = bitArray | 1 << (hash2);
-        System.out.println(getBinaryStringFromInt(bitArray) + " - бит массив после добавление хэш2 = " + hash2);
-        System.out.println();
+//        System.out.println(getBinaryStringWithShiftFromInt(bitArray) + " - бит массив после добавление хэш2 = " + hash2);
+//        System.out.println();
     }
 
     public boolean isValue(String str1) {
         // проверка, имеется ли строка str1 в фильтре
 
-        System.out.println("    ищем вхождение строки: " + str1);
+//        System.out.println("    ищем вхождение строки: " + str1);
         int hash1 = hash1(str1);
-        int hash2 = hash2(str1);
 
-        System.out.println(getBinaryStringFromInt(1 << (hash1)) + "<- хэш1 = " + hash1);
-        System.out.println(getBinaryStringFromInt(bitArray & 1 << (hash1)) + "<- после перемножения");
+//        System.out.println(getBinaryStringWithShiftFromInt(1 << (hash1)) + "<- хэш1 = " + hash1);
+//        System.out.println(getBinaryStringWithShiftFromInt(bitArray & 1 << (hash1)) + "<- после перемножения");
         if ((bitArray & 1 << (hash1)) != 0) {
-            System.out.println(getBinaryStringFromInt(1 << (hash2)) + "<- хэш2 = " + hash2);
-            System.out.println(getBinaryStringFromInt(bitArray & 1 << (hash2)) + "<- после перемножения");
-            if ((bitArray & 1 << (hash2)) != 0) {
-                return true;
-            }
+            int hash2 = hash2(str1);
+//            System.out.println(getBinaryStringWithShiftFromInt(1 << (hash2)) + "<- хэш2 = " + hash2);
+//            System.out.println(getBinaryStringWithShiftFromInt(bitArray & 1 << (hash2)) + "<- после перемножения");
+            return (bitArray & 1 << (hash2)) != 0;
         }
         return false;
     }
 
-    private String getBinaryStringFromInt(int i) {
+    private String getBinaryStringWithShiftFromInt(int i) {
         // 32 - разрядность
         String str = String.format("%32s", Integer.toBinaryString(i)).replace(" ", "0");
         str = str.replaceAll("(.{4})", "$1 ");
         return str;
-    }
-
-    public static void main(String[] args) {
-
-        BloomFilter bf = new BloomFilter(32);
-        bf.add("0123456789");
-//        bf.add("1234567890");
-
-        System.out.println("результат: " + bf.isValue("0123456789"));
-        System.out.println();
-        System.out.println("результат: " + bf.isValue("11234567890"));
-
     }
 }
--- src/test/java/algo/bloomFilter/BloomFilterTest.java
+++ src/test/java/algo/bloomFilter/BloomFilterTest.java
@@ -3,27 +3,46 @@ package algo.bloomFilter;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BloomFilterTest {
 
+    private BloomFilter bf;
+
     @Before
     public void setUp() {
-    }
-
-    @Test
-    public void hash1() {
-    }
-
-    @Test
-    public void hash2() {
-    }
-
-    @Test
-    public void add() {
+        bf = new BloomFilter(32);
     }
 
     @Test
     public void isValue() {
+        bf.add("0123456789");
+        bf.add("1234567890");
+        bf.add("2345678901");
+        bf.add("3456789012");
+        bf.add("4567890123");
+        bf.add("5678901234");
+        bf.add("6789012345");
+        bf.add("7890123456");
+        bf.add("8901234567");
+        bf.add("9012345678");
+
+        assertThat(bf.isValue("0123456789"), is(true));
+        assertThat(bf.isValue("1234567890"), is(true));
+        assertThat(bf.isValue("2345678901"), is(true));
+        assertThat(bf.isValue("3456789012"), is(true));
+        assertThat(bf.isValue("4567890123"), is(true));
+        assertThat(bf.isValue("5678901234"), is(true));
+        assertThat(bf.isValue("6789012345"), is(true));
+        assertThat(bf.isValue("7890123456"), is(true));
+        assertThat(bf.isValue("8901234567"), is(true));
+        assertThat(bf.isValue("9012345678"), is(true));
+
+        assertThat(bf.isValue("01234567891"), is(false));
+        assertThat(bf.isValue("012345678911"), is(false));
+        assertThat(bf.isValue("0123456789111"), is(false));
+        assertThat(bf.isValue("01234567891111"), is(false));
+        assertThat(bf.isValue("012345678911111"), is(false));
     }
 }
