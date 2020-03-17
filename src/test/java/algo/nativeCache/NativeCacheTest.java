558b2c0abfa12b693d86e0cfb7981d47e7ebcb60
Андреев Александр Сергеевич <ASeAndreev@sberbank.ru>
2020-03-17 10:41:10 +0300

    NativeCache - init tests

---
+++ src/test/java/algo/nativeCache/NativeCacheTest.java
@@ -0,0 +1,26 @@
+package algo.nativeCache;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NativeCacheTest {
+
+    private NativeCache nc;
+
+    @Before
+    public void setUp() {
+        nc = new NativeCache(5, String.class);
+    }
+
+    @Test
+    public void put_01() {
+
+    }
+
+    @Test
+    public void get_01() {
+
+    }
+}
